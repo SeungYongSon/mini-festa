5c1ac3f65f8d9e144eefe9029ff2bfb8528f1adb
Sungyong Son <space010127@gmail.com>
2019-07-08 11:00:33 +0900

    All the Base codes are ready

---
+++ app/src/main/java/com/kkori/mini_festa/presentation/base/BaseContract.java
@@ -0,0 +1,15 @@
+package com.kkori.mini_festa.presentation.base;
+
+public interface BaseContract {
+
+    interface Presenter<T> {
+        void createView(T view);
+
+        void destroyView();
+    }
+
+    interface View {
+        void setPresenter(Presenter<?> presenter);
+    }
+
+}
---
+++ app/src/main/java/com/kkori/mini_festa/presentation/main/MainContract.java
@@ -0,0 +1,15 @@
+package com.kkori.mini_festa.presentation.main;
+
+import com.kkori.mini_festa.presentation.base.BaseContract;
+
+interface MainContract {
+
+    interface View extends BaseContract.View {
+
+    }
+
+    interface Presenter extends BaseContract.Presenter<View> {
+
+    }
+
+}
